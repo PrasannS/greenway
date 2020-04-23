1d38612f75a89b5a13963aa40996521186781b13
ImMortal <adityaisking012@gmail.com>
2020-04-23 18:59:34 -0500

    Merge branch 'master' of https://github.com/PrasannS/greenway
    # Conflicts:
    #	android/app/google-services.json
    #	lib/main.dart
    #	pubspec.lock
    #	pubspec.yaml

--- lib/screens/gridtest_page.dart
+++ lib/screens/gridtest_page.dart
@@ -3,100 +3,108 @@ import 'package:flutter/material.dart';
 class ExampleGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Grid Demo'),
-      ),
-      body: new GridView.count(
-        crossAxisCount: 2,
-        children: new List<Widget>.generate(16, (index) {
-          return new GridTile(
-              child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 4.5),
+      return new Scaffold(
+        backgroundColor: Colors.white,
+        body:new Container(
+        child: new GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: (2 / 3),
+          children: new List<Widget>.generate(16, (index) {
+            return new GridTile(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
               child: Container(
-                  decoration: BoxDecoration(boxShadow: [
-                    BoxShadow(
-                      color: Colors.blue,
-                      offset: Offset(0, 0.1),
-                      blurRadius: 3.0,
-                    ),
-                  ], color: Colors.white, borderRadius: BorderRadius.circular(25.0)),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'Item Name',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
+                  height: 300,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.blue,
+                          offset: Offset(0, 0.1),
+                          blurRadius: 3.0,
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 10,
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.all(5),
-                      width: double.infinity,
-                      height: 100,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Image.network(
-                          'https://cdn.discordapp.com/attachments/702571621176836186/702961809275224114/images.png',
-                          fit: BoxFit.cover,
+                      SizedBox(
+                        height: 35,
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(5),
+                        width: double.infinity,
+                        height: 80,
+                        child: ClipRRect(
+                          child: Image.network(
+                            'https://cdn.discordapp.com/attachments/702571621176836186/702961809275224114/images.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.white,
+                              offset: Offset(0, 5),
+                              blurRadius: 8.0,
+                            ),
+                          ],
+                          // IMAGE GOES HERE
                         ),
                       ),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.white,
-                            offset: Offset(0, 5),
-                            blurRadius: 8.0,
-                          ),
-                        ],
-                        // IMAGE GOES HERE
+                      SizedBox(
+                        height: 35,
                       ),
-                    ),
-                    Row(children: <Widget>[
-                      Text(
-                        '   \$ 49.99',
-                        style: TextStyle(
+
+                Text('One', textAlign: TextAlign.left,
+                  style: TextStyle(
                           color: Colors.black,
                           fontSize: 20,
                           fontWeight: FontWeight.w500,
                         ),
                       ),
-                      Text(
-                        '   Eco-Friendly',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 13,
-                          fontWeight: FontWeight.w300,
+                      Row(children: <Widget>[
+                        Text(
+                          ' ___________________________',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.8,
+                          ),
                         ),
+                      ]),
+                      SizedBox(
+                        height: 5,
                       ),
-                    ]),
-                    Row(children: <Widget>[
-                      Text('   Item Description'),
-                    ]),
-                  ],
-                )),
-              )
-          );
-        }),
-      ),
-    );
+                      Row(children: <Widget>[
+                        Text(
+                          '    \$49.99',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ]),
+                      Row(children: <Widget>[
+                        Text(
+                          '     Seller',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ]),
+                    ],
+                  )),
+            ));
+          }),
+        ),
+        )
+      );
+    }
   }
-}
+
